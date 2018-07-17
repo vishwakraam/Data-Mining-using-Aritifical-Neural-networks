a63b5d1c0de8e45389776755fad05126292fd1c9
Vishwak raam Raghu <36246435+vishwakraam@users.noreply.github.com>
2018-07-17 00:02:13 -0400

    Update Data mining.py

--- Data mining.py
+++ Data mining.py
@@ -7,5 +7,5 @@ from fancyimpute import KNN
 # X is the complete data matrix
 # X_incomplete has the same values as X except a subset have been replace with NaN
 
-# Use 3 nearest rows which have a feature to fill in each row's missing features
+# 3 nearest rows which have a feature to fill in each row's missing features
 X_filled_knn = KNN(k=3).complete(X_incomplete)
